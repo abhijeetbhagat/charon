c00036173fa0626c8280b1692465ccd580734a57
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2015-09-13 15:49:16 +0530

    Add support for llvm

---
+++ src/libtrans/base.rs
@@ -0,0 +1,10 @@
+use syntax::ast::{Block, Expr};
+
+pub fn translate(expr : &Expr){
+    match expr {
+        &Expr::AddExpr(ref e1, ref e2) =>{
+            
+        },
+        _ => {}
+    }
+}
---
+++ src/libtrans/lib.rs
@@ -0,0 +1,4 @@
+extern crate syntax;
+
+
+pub mod base;
